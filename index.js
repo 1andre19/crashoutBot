c6ccecdc32fea4ff235fd9ee54841bb8d1152d3a
1andre19 <a.alfaro1@outlook.com>
2026-02-20 17:47:59 -0600

    add express json parseer

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('GET request to homepage');
 });
