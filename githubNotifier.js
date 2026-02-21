3efab095bc2e22a933229970b787ec15e287fba5
1andre19 <a.alfaro1@outlook.com>
2026-02-20 18:05:48 -0600

    change styling in notifying message

--- githubNotifier.js
+++ githubNotifier.js
@@ -1,3 +1,5 @@
+const { ContainerBuilder, MessageFlags } = require('discord.js');
+
 async function handleGithubEvent(event, payload, channel) {
     if (event !== 'push') {
         return;
@@ -6,10 +8,29 @@ async function handleGithubEvent(event, payload, channel) {
     const repo = payload.repository?.full_name;
     const pusher = payload.pusher?.name;
     const commits = payload.commits?.length || 0;
+    const commitMessage = payload.commits?.message;
 
     const message = `Push detected in ${repo} by ${pusher}. ${commits} commit(s).`;
 
-    await channel.send(message);
+    const gitPushContainer = new ContainerBuilder()
+        .setAccentColor(f2af5c)
+        .addTextDisplayComponents((textDisplay) =>
+            textDisplay.setContent(
+            `Push by **${pusher}** in repo: **${repo}**.`,
+            ),
+        )
+        .addSeparatorComponents((separator) => separator)
+        .addTextDisplayComponents((textDisplay) =>
+            textDisplay.setContent(
+            `**Message**:
+            ${commitMessage}`,
+            ),
+        );
+
+    await channel.send({
+        components: [gitPushContainer],
+        flags: MessageFlags.IsComponentsV2,
+    });
 }
 
 module.exports = { handleGithubEvent };
